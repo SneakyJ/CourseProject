c3e1fe6447b9e08bac04dc933ff312dd6359d83e
SneakyJ <k.nikolaienko@gmail.com>
2023-11-21 12:32:13 +0100

    recipe model added

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrl: './recipe-list.component.css'
 })
 export class RecipeListComponent {
+  recipes = [];
+
+  constructor() { }
+
+  ngOnInit() {
+    
+  }
 
 }
