732244d4ace9fbfac63bf98c762ee39f0d2a2fdd
SneakyJ <k.nikolaienko@gmail.com>
2023-11-21 12:45:13 +0100

    Update recipe component with content

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,12 +7,14 @@ import { Component } from '@angular/core';
   styleUrl: './recipe-list.component.css'
 })
 export class RecipeListComponent {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe('Test recipe', 'This is test recipe', 'https://bakesbybrownsugar.com/wp-content/uploads/2023/03/Sabayon-Sauce-19C.jpg.webp')
+  ];
 
   constructor() { }
 
   ngOnInit() {
-    
+
   }
 
 }
