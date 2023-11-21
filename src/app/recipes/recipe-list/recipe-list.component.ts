799541a9730560a6d8c252d0c33b406b693acbcc
SneakyJ <k.nikolaienko@gmail.com>
2023-11-21 21:01:13 +0100

    recipe detail component view added

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,6 +8,9 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent {
   recipes: Recipe[] = [
+    new Recipe('Test recipe', 'This is test recipe', 'https://bakesbybrownsugar.com/wp-content/uploads/2023/03/Sabayon-Sauce-19C.jpg.webp'),
+    new Recipe('Test recipe', 'This is test recipe', 'https://bakesbybrownsugar.com/wp-content/uploads/2023/03/Sabayon-Sauce-19C.jpg.webp'),
+    new Recipe('Test recipe', 'This is test recipe', 'https://bakesbybrownsugar.com/wp-content/uploads/2023/03/Sabayon-Sauce-19C.jpg.webp'),
     new Recipe('Test recipe', 'This is test recipe', 'https://bakesbybrownsugar.com/wp-content/uploads/2023/03/Sabayon-Sauce-19C.jpg.webp')
   ];
 
