b2f5d44b6274f8b1f42e457d385024f52d9584ee
SneakyJ <k.nikolaienko@gmail.com>
2023-11-22 22:19:40 +0100

    Added ingedient model

---
+++ src/app/shared/ingedient.model.ts
@@ -0,0 +1,11 @@
+export class Ingerient {
+    // public name: string;
+    // public amount: number;
+
+    // constructor (name: string, amount: number) {
+    //     this.name = name;
+    //     this.amount = amount;
+    // }
+
+    constructor (public name: string, public amount: number) { } // Kinda short version of commented part above
+}
